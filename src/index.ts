47e52f2a378df0680b2a6185b45edae6a9a4920b
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-03-05 15:46:15 +0200

    Refactor ceaserCipher Add type annotation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { capitalize, reverseString, calculator, ceaserCipher, analyzeArray };
 
-function capitalize(word: string) {
+function capitalize(word: string): string {
     if (!word) throw new Error("Invalid string");
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-function reverseString(word: string) {
+function reverseString(word: string): string {
     if (word === null || word === undefined) throw new Error("Invalid string");
     return word.split("").reverse().join("");
 }
@@ -17,31 +17,41 @@ const calculator = {
     divide(a: number, b: number) {
         if (b === 0) throw new Error("Cannot divide by zero");
         return a / b;
-    }
+    },
 };
 
-function ceaserCipher(word: string, shift: number) {
+function ceaserCipher(word: string, shift: number): string {
     if (word === null || word === undefined) throw new Error("Invalid string");
-    return word
-        .split("")
-        .map((letter) => {
-            let normalize: number;
-            if (letter >= "A" && letter <= "Z") {
-                normalize = 65;
-            } else if (letter >= "a" && letter <= "z") {
-                normalize = 97;
-            } else return letter;
+    let result: Array<string> = [];
 
-            return String.fromCharCode(
-                ((((letter.charCodeAt(0) + shift - normalize) % 26) + 26) %
-                    26) +
-                    normalize,
-            );
-        })
-        .join("");
+    for (let i = 0; i < word.length; i++) {
+        const letter: string = word[i];
+        let normalize: number;
+        if (letter >= "A" && letter <= "Z") {
+            normalize = 65;
+        } else if (letter >= "a" && letter <= "z") {
+            normalize = 97;
+        } else {
+            result.push(letter);
+            continue;
+        }
+
+        result.push(String.fromCharCode(
+            ((((letter.charCodeAt(0) + shift - normalize) % 26) + 26) % 26) +
+                normalize,
+        ));
+    }
+    return result.join("");
 }
 
-function analyzeArray(arr: Array<number>) {
+interface Analysis {
+    max: number,
+    min: number,
+    average: number,
+    length: number
+}
+
+function analyzeArray(arr: Array<number>): Analysis {
     if (arr.length === 0) {
         return { max: null, min: null, average: null, length: 0 };
     }
